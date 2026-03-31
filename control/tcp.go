5e8bc32651c66e67793c11dabe4a5063a2e738d2
amtoaer <amtoaer@gmail.com>
2026-03-31 18:12:50 +0800

    触发重新测速时打印日志

--- control/tcp.go
+++ control/tcp.go
@@ -59,7 +59,18 @@ func (c *ControlPlane) handleConn(lConn net.Conn) (err error) {
 	if err != nil {
 		if d != nil {
 			if strings.Contains(err.Error(), "connection refused") {
+				c.log.WithFields(logrus.Fields{
+					"dialer":  d.Property().Name,
+					"network": networkType.String(),
+					"err":     err,
+				}).Warnln("Dial failed with connection refused, marking dialer as unavailable and re-checking")
 				d.ReportUnavailable(networkType, err)
+			} else {
+				c.log.WithFields(logrus.Fields{
+					"dialer":  d.Property().Name,
+					"network": networkType.String(),
+					"err":     err,
+				}).Warnln("Dial failed, re-checking dialer")
 			}
 			d.NotifyCheck()
 		}
